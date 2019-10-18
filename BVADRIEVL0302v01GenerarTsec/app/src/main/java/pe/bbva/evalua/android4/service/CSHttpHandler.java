3d706ceced7faa422965975785f0212cdd630c46
Alex Luis Díaz Vásquez <alex.diaz.vasquez@bbva.com>
2019-10-18 17:14:09 -0500

    Generar tsec

--- BVADRIEVL0302v01GenerarTsec/app/src/main/java/pe/bbva/evalua/android4/service/CSHttpHandler.java
+++ BVADRIEVL0302v01GenerarTsec/app/src/main/java/pe/bbva/evalua/android4/service/CSHttpHandler.java
@@ -2,11 +2,17 @@ package pe.bbva.evalua.android4.service;
 
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -14,74 +20,100 @@ import java.net.URL;
 
 public class CSHttpHandler {
     private static final String TAG = CSHttpHandler.class.getSimpleName();
+    private URL oUrl;
+    private HttpURLConnection oHttpUrlCxn = null;
 
-    public CSHttpHandler(){}
+    public CSHttpHandler() {
+    }
 
-    public String makeServiceCall(String psRequestUrl){
+    public String makeServiceCall(String psRequestUrl) {
         String lsResponse = null;
-/*
-POST https://des.wallet.bbvacontinental.pe/QSRV_A02/TechArchitecture/pe/grantingTicket/V02
 
-PAYLOAD:
-{
-  "authentication": {
-      "userID": "L38140100",
-      "consumerID": "13999901",
-      "authenticationType": "02",
-      "authenticationData": [
-          {
-              "idAuthenticationData": "password",
-              "authenticationData": [
-                  "wallet01"
-              ]
-          }
-      ]
-  },
-  "backendUserRequest": {
-      "userId":"",
-      "accessCode": "",
-      "dialogId": ""
-  }
-}
+        
+        try {
+            oUrl = new URL(psRequestUrl);
+            oHttpUrlCxn = (HttpURLConnection) oUrl.openConnection();
 
-respuesta: cabecera "tsec"
- */
-        try{
-            URL oUrl = new URL(psRequestUrl);
-            HttpURLConnection oHttpUrlCxn = (HttpURLConnection) oUrl.openConnection();
+            // Add request Header
             oHttpUrlCxn.setRequestMethod("POST");
-            // Read the response
-            InputStream oIstream = new BufferedInputStream(oHttpUrlCxn.getInputStream());
-            lsResponse = convertStreamToString(oIstream);
-        }catch(MalformedURLException ex){
+            oHttpUrlCxn.setRequestProperty("Content-Type", "application/json");
+            oHttpUrlCxn.setRequestProperty("Accept", "application/json");
+            oHttpUrlCxn.connect();
+
+            //Add Post Data in JSON
+            JSONObject oJsonData = new JSONObject();
+
+            JSONObject oJsonAuthentication = new JSONObject();
+            oJsonAuthentication.put("userID", "L38140100");
+            oJsonAuthentication.put("consumerID", "13999901");
+            oJsonAuthentication.put("authenticationType", "02");
+
+            JSONArray oJsonArrayNivel0 = new JSONArray();
+
+            JSONObject oJsonPassword = new JSONObject();
+            oJsonPassword.put("idAuthenticationData", "password");
+            JSONArray oJsonArrayNivel1 = new JSONArray();
+            oJsonArrayNivel1.put("wallet01");
+            oJsonPassword.put("authenticationData", oJsonArrayNivel1);
+            oJsonArrayNivel0.put(oJsonPassword);
+
+            oJsonAuthentication.put("authenticationData", oJsonArrayNivel0);
+            oJsonData.put("authentication", oJsonAuthentication);
+
+            JSONObject oJsonBackendUserRequest = new JSONObject();
+            oJsonBackendUserRequest.put("userId", "");
+            oJsonBackendUserRequest.put("accessCode", "");
+            oJsonBackendUserRequest.put("dialogId", "");
+
+            oJsonData.put("backendUserRequest", oJsonBackendUserRequest);
+
+            //Create a writer object and make the request
+            OutputStream oOStream = oHttpUrlCxn.getOutputStream();
+            OutputStreamWriter oOStremWriter = new OutputStreamWriter(oOStream);
+            BufferedWriter oBWriter = new BufferedWriter(oOStremWriter);
+            oBWriter.write(oJsonData.toString());
+            oBWriter.flush();
+            oBWriter.close();
+
+            //Get the Response code for the request
+
+            int liResponseCode = oHttpUrlCxn.getResponseCode();
+
+            if (liResponseCode==HttpURLConnection.HTTP_OK){
+                InputStream oIstream = new BufferedInputStream(oHttpUrlCxn.getInputStream());
+                convertStreamToString(oIstream);
+            }
+        } catch (MalformedURLException ex) {
             Log.e(TAG, "MalformedURLException : " + ex.getMessage());
-        }catch (ProtocolException ex){
+        } catch (ProtocolException ex) {
             Log.e(TAG, "ProtocolException : " + ex.getMessage());
-        }catch (IOException ex){
+        } catch (IOException ex) {
             Log.e(TAG, "IOException : " + ex.getMessage());
-        }catch (Exception ex){
+        } catch (Exception ex) {
             Log.e(TAG, "Exception : " + ex.getMessage());
+        }finally {
+            oHttpUrlCxn.disconnect();
         }
         return lsResponse;
     }
 
-    public String convertStreamToString(InputStream poIstream){
+    public String convertStreamToString(InputStream poIstream) {
         InputStreamReader oIStreamReader = new InputStreamReader(poIstream);
         BufferedReader oBReader = new BufferedReader(oIStreamReader);
         StringBuilder oSBuilder = new StringBuilder();
         String lsRow;
 
-        try{
-            while ((lsRow=oBReader.readLine())!=null){
+        try {
+            while ((lsRow = oBReader.readLine()) != null) {
                 oSBuilder.append(lsRow).append("\n");
             }
 
-        }catch(IOException ex){
+        } catch (IOException ex) {
             Log.e(TAG, "IOException : " + ex.getMessage());
-        }finally {
-            try{
+        } finally {
+            try {
                 poIstream.close();
-            }catch (IOException ex){
+            } catch (IOException ex) {
                 Log.e(TAG, "IOException : " + ex.getMessage());
             }
         }
--- BVADRIEVL0302v01GenerarTsec/app/src/main/java/pe/bbva/evalua/android4/view/CV0101v01GenerarTsec.java
+++ BVADRIEVL0302v01GenerarTsec/app/src/main/java/pe/bbva/evalua/android4/view/CV0101v01GenerarTsec.java
@@ -2,14 +2,13 @@ package pe.bbva.evalua.android4.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.ConnectivityManager;
+import android.net.Network;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListAdapter;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,7 +16,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
 
 import pe.bbva.evalua.android4.service.CSHttpHandler;
 
@@ -34,28 +32,37 @@ public class CV0101v01GenerarTsec extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        oEtUsuario.findViewById(R.id.id_edit_text_usuario);
+/*        oEtUsuario.findViewById(R.id.id_edit_text_usuario);
         oEtPassword.findViewById(R.id.id_edit_text_password);
         oTvNumeroTsec.findViewById(R.id.id_text_view_tsec);
         oBtnGenerarTsec.findViewById(R.id.id_button_generar_tsec);
 
         oEtUsuario.setText("L38140100");
-        oEtPassword.setText("wallet01");
+        oEtPassword.setText("wallet01");*/
 
-        oBtnGenerarTsec.setOnClickListener(new View.OnClickListener(){
+/*
+    Revisar si hay conexion a internet
+        ConnectivityManager oConnectivity = (ConnectivityManager) this.getSystemService(CONNECTIVITY_SERVICE);
+        Network oInformatioNetwork = oConnectivity.getActiveNetwork();
+*/
+
+        getTsec oCallPost = new getTsec();
+        oCallPost.execute();
+
+
+        /*oBtnGenerarTsec.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                new getTsec().execute();
                 oTvNumeroTsec.setText(sRptaTsec);
             }
-        });
+        });*/
     }
 
     private class getTsec extends AsyncTask<Void, Void, Void> {
         @Override
         protected void onPreExecute() {
-            String lsMensaje = "Json Data is downloading .....!";
+            String lsMensaje = "Going for the network call.. .....!";
             super.onPreExecute();
             Toast.makeText(CV0101v01GenerarTsec.this, lsMensaje, Toast.LENGTH_LONG).show();
         }
